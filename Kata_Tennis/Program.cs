69f370c34efded5ef830351ab62e55c6b4b2a636
RonEerdekensPXL <ron.eerdekens@student.pxl.be>
2024-11-05 20:24:02 +0100

    Add project files.

---
+++ Kata_Tennis/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kata_Tennis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

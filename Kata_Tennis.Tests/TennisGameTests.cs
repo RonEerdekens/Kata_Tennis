89546d180e821ab07bf2372930aa1c4e0f6eab11
RonEerdekensPXL <ron.eerdekens@student.pxl.be>
2024-11-05 20:31:53 +0100

    GivenPlayerOneScored_WhenScoreIsLoveLove_ThenScoreShouldBeFifteenLove Test + fake it solution

---
+++ Kata_Tennis.Tests/TennisGameTests.cs
@@ -0,0 +1,26 @@
+﻿using NuGet.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Tennis.Tests
+{
+    [TestFixture]
+    public class TennisGameTests
+    {
+        [Test]
+        public void GivenPlayerOneScored_WhenScoreIsLoveLove_ThenScoreShouldBeFifteenLove()
+        {
+            // Arrange
+            var game = new TennisGame("Player1", "Player2");
+
+            // Act
+            var actual = game.PrintScore();
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("Fifteen-Love"));
+        }
+    }
+}
---
+++ Kata_Tennis/TennisGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Tennis
+{
+    public class TennisGame
+    {
+        private readonly string _player1Name;
+        private readonly string _player2Name;
+        private int _player1Score;
+        private int _player2Score;
+
+
+        public TennisGame(string player1Name, string player2Name)
+        {
+            _player1Name = player1Name;
+            _player2Name = player2Name;
+            _player1Score = 0;
+            _player2Score = 0;
+        }
+
+        public string PrintScore()
+        {
+            return "Fifteen-Love";
+        }
+    }
+}
